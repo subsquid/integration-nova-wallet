2b1e64d03b10024ca40300298285139526cc3f69
sachushaji <sachushajiabraham@gmail.com>
2021-11-01 07:25:55 +0000

    declaring addtional types

--- src/types/Balances.ts
+++ src/types/Balances.ts
@@ -1,7 +1,7 @@
 import {create} from './_registry'
-import {AccountId32} from '@polkadot/types/interfaces'
-import {u128} from '@polkadot/types'
-import {SubstrateEvent} from '@subsquid/hydra-common'
+import {AccountId32, MultiAddress} from '@polkadot/types/interfaces'
+import {Compact, u128} from '@polkadot/types'
+import {SubstrateEvent, SubstrateExtrinsic} from '@subsquid/hydra-common'
 
 export namespace Balances {
   /**
@@ -16,7 +16,8 @@ export namespace Balances {
   }
 
   /**
-   * Some amount was deposited (e.g. for transaction fees). \[who, deposit\]
+   * Some amount was deposited into the account (e.g. for transaction fees). \[who,
+   * deposit\]
    */
   export class DepositEvent {
     constructor(private event: SubstrateEvent) {}
@@ -26,4 +27,32 @@ export namespace Balances {
     }
   }
 
+  /**
+   * Same as the [`transfer`] call, but with a check that the transfer will not kill the
+   * origin account.
+   * 
+   * 99% of the time you want [`transfer`] instead.
+   * 
+   * [`transfer`]: struct.Pallet.html#method.transfer
+   * # <weight>
+   * - Cheaper than transfer because account cannot be killed.
+   * - Base Weight: 51.4 µs
+   * - DB Weight: 1 Read and 1 Write to dest (sender is in overlay already)
+   * #</weight>
+   */
+  export class Transfer_keep_aliveCall {
+    private _extrinsic: SubstrateExtrinsic
+
+    constructor(extrinsic: SubstrateExtrinsic) {
+      this._extrinsic = extrinsic
+    }
+
+    get dest(): MultiAddress {
+      return create('MultiAddress', this._extrinsic.args[0].value)
+    }
+
+    get value(): Compact<u128> {
+      return create('Compact<u128>', this._extrinsic.args[1].value)
+    }
+  }
 }
--- src/types/Staking.ts
+++ src/types/Staking.ts
@@ -1,7 +1,7 @@
 import {create} from './_registry'
 import {AccountId32} from '@polkadot/types/interfaces'
-import {u128} from '@polkadot/types'
-import {SubstrateEvent} from '@subsquid/hydra-common'
+import {u128, u32} from '@polkadot/types'
+import {SubstrateEvent, SubstrateExtrinsic} from '@subsquid/hydra-common'
 
 export namespace Staking {
   /**
@@ -63,4 +63,60 @@ export namespace Staking {
     }
   }
 
+  /**
+   * Pay out all the stakers behind a single validator for a single era.
+   * 
+   * - `validator_stash` is the stash account of the validator. Their nominators, up to
+   *   `T::MaxNominatorRewardedPerValidator`, will also receive their rewards.
+   * - `era` may be any era between `[current_era - history_depth; current_era]`.
+   * 
+   * The origin of this call must be _Signed_. Any account can call this function, even if
+   * it is not one of the stakers.
+   * 
+   * # <weight>
+   * - Time complexity: at most O(MaxNominatorRewardedPerValidator).
+   * - Contains a limited number of reads and writes.
+   * -----------
+   * N is the Number of payouts for the validator (including the validator)
+   * Weight:
+   * - Reward Destination Staked: O(N)
+   * - Reward Destination Controller (Creating): O(N)
+   * 
+   *   NOTE: weights are assuming that payouts are made to alive stash account (Staked).
+   *   Paying even a dead controller is cheaper weight-wise. We don't do any refunds here.
+   * # </weight>
+   */
+  export class Payout_stakersCall {
+    private _extrinsic: SubstrateExtrinsic
+
+    constructor(extrinsic: SubstrateExtrinsic) {
+      this._extrinsic = extrinsic
+    }
+
+    get validator_stash(): AccountId32 {
+      return create('AccountId32', this._extrinsic.args[0].value)
+    }
+
+    get era(): u32 {
+      return create('u32', this._extrinsic.args[1].value)
+    }
+  }
+  // Custom type for payout validator call
+
+export class Payout_validatorCall {
+  private _extrinsic: SubstrateExtrinsic
+
+  constructor(extrinsic: SubstrateExtrinsic) {
+    this._extrinsic = extrinsic
+  }
+  get era(): u32 {
+    return create('u32', this._extrinsic.args[1].value)
+  }
 }
+}
+
+
+
+
+
+
