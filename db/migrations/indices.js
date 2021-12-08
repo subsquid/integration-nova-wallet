24a8cefd77a43b3a344292ae7adca3b67d0c8210
Sachu Shaji Abraham <sachushajiabraham@gmail.com>
2021-12-09 01:21:59 +0530

    enhancement: add missing indexes (#11)
    * add missing indexes
    
    * update indices to a seperate file
    
    * Remove drop queries from migration

---
+++ db/migrations/indices.js
@@ -0,0 +1,25 @@
+const { MigrationInterface, QueryRunner } = require("typeorm");
+
+module.exports = class nova1638846580628 {
+    name = 'nova1638846580628'
+
+    async up(queryRunner) {
+        await queryRunner.query(`CREATE INDEX transfer_from on transfer ("from")`);
+        await queryRunner.query(`CREATE INDEX transfer_to on transfer ("to")`);
+
+        await queryRunner.query(`CREATE INDEX account_history_block_number on account_history (block_number)`);
+        await queryRunner.query(`CREATE INDEX account_history_address on account_history (address)`);
+
+        await queryRunner.query(`CREATE INDEX era_validator_era on era_validator_info (era)`);
+        await queryRunner.query(`CREATE INDEX era_validator_info_address on era_validator_info (address)`);
+
+
+        await queryRunner.query(`CREATE INDEX stake_change_address on stake_change (address)`);
+        await queryRunner.query(`CREATE INDEX stake_change_block_number on stake_change (block_number)`);
+
+    }
+
+    async down(queryRunner) {
+      
+    }
+}
